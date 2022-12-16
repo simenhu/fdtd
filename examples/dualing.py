28490e46dcd3009c5f071e046e40afb8e6814e35
Bijan Fakhri <bfakhri@asu.edu>
2022-12-16 09:35:53 -0600

    U

--- examples/dualing.py
+++ examples/dualing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('/home/bij/PersonalProjects/fdtd/')
+sys.path.append('/home/bij/Projects/fdtd/')
 import fdtd
 import fdtd.backend as bd
 import matplotlib.pyplot as plt
