bda51aab8b931926b679dafcdb5d62fa034de823
Bijan Fakhri <bfakhri@asu.edu>
2022-12-16 09:35:43 -0600

    U

---
+++ examples/simulation_benchmark.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('/home/bij/Projects/fdtd/')
+import fdtd
+import fdtd.backend as bd
+import matplotlib.pyplot as plt
+import time
+
+
+# ## Set Backend
+#fdtd.set_backend("numpy")
+fdtd.set_backend("torch")
+
+
+# ## Constants
+WAVELENGTH = 1550e-9
+WAVELENGTH2 = 1550e-8
+SPEED_LIGHT: float = 299_792_458.0  # [m/s] speed of light
+
+
+# ## Simulation
+
+# create FDTD Grid
+
+# In[4]:
+
+
+grid = fdtd.Grid(
+    (1.5e-5, 1.5e-5, 1),
+    grid_spacing=0.1 * WAVELENGTH,
+    permittivity=1.0,
+    permeability=1.0,
+)
+
+print('Grid Shape: ', grid.shape)
+
+
+# boundaries
+
+# In[5]:
+
+
+# grid[0, :, :] = fdtd.PeriodicBoundary(name="xbounds")
+grid[0:10, :, :] = fdtd.PML(name="pml_xlow")
+grid[-10:, :, :] = fdtd.PML(name="pml_xhigh")
+
+# grid[:, 0, :] = fdtd.PeriodicBoundary(name="ybounds")
+grid[:, 0:10, :] = fdtd.PML(name="pml_ylow")
+grid[:, -10:, :] = fdtd.PML(name="pml_yhigh")
+
+grid[:, :, 0] = fdtd.PeriodicBoundary(name="zbounds")
+
+
+# sources
+
+# grid[50, 70:75, 0] = fdtd.LineSource(
+#     period=WAVELENGTH / SPEED_LIGHT, name="linesource"
+# )
+# grid[70, 70:75, 0] = fdtd.LineSource(
+#     period=WAVELENGTH / SPEED_LIGHT, name="linesource2",
+# )
+
+grid[20, -20:-25, 0] = fdtd.LineSource(
+    period=WAVELENGTH / SPEED_LIGHT, name="linesource0"
+)
+grid[-20, -20:-25, 0] = fdtd.LineSource(
+    period=WAVELENGTH / SPEED_LIGHT, name="linesource1"
+)
+grid[20, 20:25, 0] = fdtd.LineSource(
+    period=WAVELENGTH / SPEED_LIGHT, name="linesource2",
+)
+grid[-20, 20:25, 0] = fdtd.LineSource(
+    period=WAVELENGTH / SPEED_LIGHT, name="linesource3",
+)
+
+
+# detectors
+
+# grid[12e-6, :, 0] = fdtd.LineDetector(name="detector")
+
+
+# objects
+
+midpoint_y = grid.shape[0]//2
+midpoint_x = grid.shape[1]//2
+grid[midpoint_y-10:midpoint_y+10, midpoint_x-10:midpoint_x+10, 0:1] = fdtd.AnisotropicObject(permittivity=2.5, name="object")
+
+
+# ## Run simulation
+
+# ## Visualization
+
+
+print('Done initializing everything, starting sim....')
+time_beg = time.perf_counter()
+
+grid.visualize(z=0, animate=True)
+for i in range(100):
+    grid.run(100, progress_bar=False)
+    grid.visualize(z=0, norm='log', animate=True)
+    plt.show()
+time_end = time.perf_counter()
+
+
+print('Simulation ended, it took {0:f}s to end'.format(time_end - time_beg))
+
+
+
