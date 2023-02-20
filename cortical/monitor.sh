0a6d93badfe856f3d0c45dedc10929ab84fa2074
Bijan Fakhri <bfakhri@asu.edu>
2023-02-20 16:26:26 -0600

    Made this script just a monitoring script, no training

--- cortical/monitor.sh
+++ cortical/monitor.sh
@@ -1,11 +1,10 @@
 #!/bin/sh 
-tmux new-session -s 'train' -d 'python resonate_all.py'
-tmux split-window -v 'watch -n 0.1 nvidia-smi'
+#tmux new-session -s 'train' -d 'python resonate_all.py --load-step latest'
+tmux new-session -s 'train' -d 'watch -n 0.1 nvidia-smi'
 tmux split-window -v 'htop'
 tmux rename-window 'Monitor'
 tmux set -g remain-on-exit on
 tmux new-window 'tensorboard --logdir=./runs/ --port=12345'
-tmux rename-window 'Tensorboard'
-tmux select-window -t 0
+#tmux select-window -t 0
 tmux -2 attach-session -d 
 
