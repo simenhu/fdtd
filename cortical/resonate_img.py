8b20ebcda593f701b204e5be9f93cf48ae77a257
Bijan Fakhri <bfakhri@asu.edu>
2023-02-22 10:46:03 -0600

    Added an image resonator - let's see if it'll learn

---
+++ cortical/resonate_img.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+import git
+import sys
+import datetime
+from pathlib import Path
+sys.path.append('/home/bij/Projects/fdtd/')
+import math
+import time
+import fdtd
+import fdtd.backend as bd
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.optim as optim
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+from autoencoder import AutoEncoder
+import argparse
+from os import listdir
+from os.path import isfile, join
+
+parser = argparse.ArgumentParser(description='Process args.')
+parser.add_argument('-l', '--load-step', type=str, default='0',
+                    help='Where to start training. If latest, will start at the latest checkpoint.')
+parser.add_argument('-s', '--save-steps', type=int, default='1000',
+                    help='How often to save the model.')
+parser.add_argument('-m', '--max-steps', type=int, default='1000000000000000',
+                    help='How many steps to train.')
+args = parser.parse_args()
+
+#TODO - move this to a util file next cleanup
+def get_sample_img(img_loader, color=True):
+    _, (example_datas, labels) = next(enumerate(img_loader))
+    if(color):
+        sample = example_datas[0]
+        sample = sample.to(device)[None, :]
+    else:
+        sample = example_datas[0][0]
+        sample = sample.to(device)[None, None, :]
+    return sample
+
+def get_object_by_name(grid, name):
+    for obj in grid.objects:
+        if(obj.name == name):
+            return obj
+
+def norm_img_by_chan(img):
+    '''
+    Puts each channel into the range [0,1].
+    Expects input to be in CHW config.
+    '''
+    img_flat = torch.reshape(img, (3, -1))
+    chan_maxes, _ = torch.max(img_flat, dim=-1, keepdims=True) 
+    chan_mins, _  = torch.min(img_flat, dim=-1, keepdims=True) 
+    chans_dynamic_range = chan_maxes - chan_mins
+    normed_img = (img - chan_mins[...,None])/(chans_dynamic_range[...,None])
+    return normed_img 
+
+# Setup tensorboard
+tb_parent_dir = './runs/'
+repo = git.Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
+#head = repo.head
+local_branch = repo.active_branch.name
+run_dir = local_branch + '/' + sha[-3:] + '/' +  datetime.datetime.now().isoformat(timespec='seconds') + '/'
+print('TB Log Directory is: ', tb_parent_dir + run_dir)
+writer = SummaryWriter(log_dir=tb_parent_dir + run_dir)
+
+# Setup model saving
+model_parent_dir = './model_checkpoints/'
+model_checkpoint_dir = model_parent_dir + local_branch + '/'
+path = Path(model_checkpoint_dir)
+path.mkdir(parents=True, exist_ok=True)
+
+# ## Set Backend
+backend_name = "torch"
+#backend_name = "torch.cuda.float32"
+#backend_name = "torch.cuda.float64"
+fdtd.set_backend(backend_name)
+if(backend_name.startswith("torch.cuda")):
+    device = "cuda"
+else:
+    device = "cpu"
+
+image_transform = torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor()])
+train_dataset = torchvision.datasets.CIFAR10('cifar10/', 
+                                           train=True, 
+                                           download=True,
+                                           transform=image_transform)
+#TODO - turn SHUFFLE back to TRUE for training on multiple images.
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size=1, 
+                                           shuffle=True)
+
+
+sample = get_sample_img(train_loader)
+print('Image shape: ', sample.shape)
+ih, iw = tuple(sample.shape[2:4])
+
+# Physics constants
+WAVELENGTH = 1550e-9 # meters
+SPEED_LIGHT: float = 299_792_458.0  # [m/s] speed of light
+GRID_SPACING = 0.1 * WAVELENGTH # meters
+
+
+
+# Size of grid boundary layer
+bw = 10
+# Create FDTD Grid
+grid_h, grid_w = (ih+bw*2, iw+bw*2)
+# Boundaries with width bw
+grid = fdtd.Grid(
+    (grid_h, grid_w, 1),
+    grid_spacing=GRID_SPACING,
+    permittivity=1.0,
+    permeability=1.0,
+)
+
+# Calculate how long it takes a wave to cross the entire grid.
+grid_diag_cells = math.sqrt(grid_h**2 + grid_w**2)
+grid_diag_len = grid_diag_cells * GRID_SPACING
+grid_diag_steps = int(grid_diag_len/SPEED_LIGHT/grid.time_step)+1
+print('Time Steps to Cover Entire Grid: ', grid_diag_steps)
+
+
+# Create learnable objects at the boundaries
+grid[  0: bw, :, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="xlow")
+grid[-bw:   , :, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="xhigh")
+grid[:,   0:bw, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="ylow")
+grid[:, -bw:  , :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="yhigh")
+grid[:, :, 0] = fdtd.PeriodicBoundary(name="zbounds")
+
+# Creat the cortical column sources
+grid[bw:bw+ih,bw:bw+iw,0] = fdtd.CorticalColumnPlaneSource(
+    period = WAVELENGTH / SPEED_LIGHT,
+    polarization = 'x', # BS value, polarization is not used.
+    name='cc'
+)
+
+# Object defining the cortical column substrate 
+grid[bw:-bw, bw:-bw, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="cc_substrate")
+
+# List all model checkpoints
+checkpoints = [f for f in listdir(model_checkpoint_dir) if(isfile(join(model_checkpoint_dir, f)) and f.endswith('.pt'))]
+# Get the latest checkpoint
+model = AutoEncoder(grid=grid, input_chans=1, output_chans=1).to(device)
+checkpoint_steps = [int(cf.split('_')[-1].split('.')[0]) for cf in checkpoints]
+if(args.load_step == 'latest'):
+    if(len(checkpoint_steps) > 0):
+        latest_idx = np.argmax(checkpoint_steps)
+        start_step = checkpoint_steps[latest_idx]
+        model_dict_path = model_checkpoint_dir + checkpoints[latest_idx]
+        print('Loading model {0}.'.format(model_dict_path))
+        model.load_state_dict(torch.load(model_dict_path))
+    else:
+        start_step = 0
+elif(int(args.load_step) != 0):
+    if(int(args.load_step) not in checkpoint_steps):
+        print('Checkpoint {0} not found in {1}'.format(args.load_step, model_checkpoint_dir))
+        sys.exit()
+    start_step = int(args.load_step)
+    model_idx = np.where(np.array(checkpoint_steps) == start_step)[0][0]
+    model_dict_path = model_checkpoint_dir + checkpoints[model_idx]
+    print('Loading model {0}.'.format(model_dict_path))
+    model.load_state_dict(torch.load(model_dict_path))
+else:
+    print('Starting model at step 0')
+    start_step = 0
+
+def toy_img(img):
+    img = torch.zeros_like(img)
+    x, y, b, s = np.random.rand(4)
+    max_size = 14
+    min_size =  6
+    max_b = 1.0
+    min_b = 0.5
+    x = int(x*(img.shape[-1] - max_size))
+    y = int(y*(img.shape[-2] - max_size))
+    b = float(min_b + b*(max_b - min_b))
+    s = int(min_size + s*(max_size - min_size))
+    img[..., x:x+s, y:y+s] = b
+    return bd.array(img[:,0,...])
+
+print('All grid objects: ', [obj.name for obj in grid.objects])
+params_to_learn = []
+params_to_learn += [get_object_by_name(grid, 'xlow').inverse_permittivity]
+params_to_learn += [get_object_by_name(grid, 'xhigh').inverse_permittivity]
+params_to_learn += [get_object_by_name(grid, 'ylow').inverse_permittivity]
+params_to_learn += [get_object_by_name(grid, 'yhigh').inverse_permittivity]
+#TODO - disabling substrate learning for now
+params_to_learn += [get_object_by_name(grid, 'cc_substrate').inverse_permittivity]
+params_to_learn += [*model.parameters()]
+
+# Optimizer params
+optimizer = optim.AdamW(params_to_learn, lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False)
+mse = torch.nn.MSELoss(reduce=False)
+loss_fn = torch.nn.MSELoss()
+
+em_steps = 200
+
+grid.H.requires_grad = True
+grid.H.retain_grad()
+grid.E.requires_grad = True
+grid.E.retain_grad()
+
+# For timing steps
+stopwatch = time.time()
+
+# Train the weights
+for train_step in range(start_step + 1, start_step + args.max_steps):
+    # Generate a new image
+    #img = toy_img(orig_img)
+    img = get_sample_img(train_loader, color=False)
+
+    # Reset grid and optimizer
+    grid.reset()
+    optimizer.zero_grad()
+    # Push it through Encoder
+    #if((train_step % 100 == 0) and (train_step > 0)):
+    if((train_step % 500 == 0) and (train_step > 0)):
+    #if((train_step % 500 == 0)):
+        vis = True
+    else:
+        vis = False
+
+    num_samples = 1
+    # Get sample from training data
+    img_hat_em, _, em_field = model(img, em_steps=em_steps, visualize=vis)
+    e_field_img = em_field[:, 0:3,...]
+    h_field_img = em_field[:, 3:6,...]
+
+    # Add images to tensorboard
+    for s in range(num_samples):
+        #img_trip_chan = bd.stack([img, img, img])
+        #img_grid = torchvision.utils.make_grid([img[0,...], img_hat_em[s]])
+        img_grid = torchvision.utils.make_grid([img[0,...], img_hat_em])
+        #img_grid = torchvision.utils.make_grid([img, img_hat_em])
+            #norm_img_by_chan(e_field_img[s]), 
+            #norm_img_by_chan(h_field_img[s])])
+        writer.add_image('sample_'+str(s), img_grid, train_step)
+
+    perm = torch.reshape(get_object_by_name(grid, 'cc_substrate').inverse_permittivity, (-1, 32, 32))
+    writer.add_image('ccsubstrate1', perm[0:3,...], train_step)
+    writer.add_image('ccsubstrate2', perm[3:6,...], train_step)
+    writer.add_image('ccsubstrate3', perm[6:9,...], train_step)
+
+    # Generate loss
+    loss = loss_fn(img_hat_em, img) 
+
+    writer.add_scalar('Total Loss', loss, train_step)
+    writer.add_scalar('em_steps', em_steps, train_step)
+    writer.add_scalar('ccsubstate_sum', 
+            torch.sum(get_object_by_name(grid, 'cc_substrate').inverse_permittivity), train_step)
+
+    print('Step: ', train_step, '\tTime: ', grid.time_steps_passed, '\tLoss: ', loss)
+
+    # Tensorboard
+    writer.add_histogram('cc_dirs', model.cc_dirs, train_step)
+    writer.add_histogram('cc_freqs', model.cc_freqs, train_step)
+    writer.add_histogram('cc_phases', model.cc_phases, train_step)
+    writer.add_histogram('ccsubstrate', get_object_by_name(grid, 'cc_substrate').inverse_permittivity, train_step)
+    writer.add_histogram('xlow', get_object_by_name(grid, 'xlow').inverse_permittivity, train_step)
+    writer.add_histogram('xhigh', get_object_by_name(grid, 'xhigh').inverse_permittivity, train_step)
+    writer.add_histogram('ylow', get_object_by_name(grid, 'ylow').inverse_permittivity, train_step)
+    writer.add_histogram('yhigh', get_object_by_name(grid, 'yhigh').inverse_permittivity, train_step)
+    writer.add_histogram('e_field', e_field_img, train_step)
+    writer.add_histogram('h_field', h_field_img, train_step)
+
+    optimizer.zero_grad()
+    # Backprop
+    loss.backward(retain_graph=True)
+    optimizer.step()
+
+    # Save model 
+    if((train_step % args.save_steps == 0) and (train_step > 0)):
+        torch.save(model.state_dict(), model_checkpoint_dir + 'md_'+str(train_step).zfill(12)+'.pt')
+
+    # Profile performance
+    seconds_per_step = time.time() - stopwatch 
+    writer.add_scalar('seconds_per_step', torch.tensor(seconds_per_step), train_step)
+    stopwatch = time.time()
+
+writer.close()
