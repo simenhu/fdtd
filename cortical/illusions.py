beb34bc8d70fb0a7c3408bb1c04fc02a42d1a013
Bijan Fakhri <bfakhri@asu.edu>
2023-06-06 00:57:02 -0500

    New illusions

--- cortical/illusions.py
+++ cortical/illusions.py
@@ -10,23 +10,28 @@ import fdtd
 import fdtd.backend as bd
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import torch
 import torch.optim as optim
 import torchvision
 from torchvision.utils import save_image
 from torch.utils.tensorboard import SummaryWriter
-from  torch.nn.modules.upsampling import Upsample
+import scipy
 from autoencoder import AutoEncoder
 import argparse
 from os import listdir
 from os.path import isfile, join
-import cv2
 from PIL import Image
-
 
 parser = argparse.ArgumentParser(description='Process args.')
 parser.add_argument('-f', '--load-file', type=str, default=None,
                     help='File to load params from before training starts. Overrides --load-step.')
+parser.add_argument('-c', '--coverage-ratio', type=float, default=1.0,
+                    help='How much distance a wave can cover as a proportion of the diagonal length of the sim.')
+parser.add_argument('-is', '--image-size', type=int, default=40,
+                    help='Size of each side of the image. Determines grid size.')
+parser.add_argument('-sc', '--image-scaler', type=int, default=1,
+                    help='How much to scale the entire simulation by (changes the dimensions of the model).')
 args = parser.parse_args()
 
 def get_sorted_paths(directory_list, target_ext='.png'):
@@ -40,6 +45,32 @@ def get_sorted_paths(directory_list, target_ext='.png'):
 
 img_paths = get_sorted_paths(['./optical_illusions/'])
 
+#TODO - move this to a util file next cleanup
+def get_sample_img(img_loader, color=True):
+    _, (example_datas, labels) = next(enumerate(img_loader))
+    if(color):
+        sample = example_datas[0]
+        sample = sample.to(device)[None, :]
+    else:
+        sample = example_datas[0][0]
+        sample = sample.to(device)[None, None, :]
+    return sample
+
+def get_object_by_name(grid, name):
+    for obj in grid.objects:
+        if(obj.name == name):
+            return obj
+    else:
+        print('Could not find object: ', name)
+        sys.exit()
+def get_source_by_name(grid, name):
+    for src in grid.sources:
+        if(src.name == name):
+            return src 
+    else:
+        print('Could not find object: ', name)
+        sys.exit()
+
 def norm_img_by_chan(img):
     '''
     Puts each channel into the range [0,1].
@@ -52,15 +83,13 @@ def norm_img_by_chan(img):
     normed_img = (img - chan_mins[...,None])/(chans_dynamic_range[...,None])
     return normed_img 
 
-def get_object_by_name(grid, name):
-    for obj in grid.objects:
-        if(obj.name == name):
-            return obj
-def get_source_by_name(grid, name):
-    for src in grid.sources:
-        if(src.name == name):
-            return src 
+class RandomRot90:
+    # Randomly rotates the image by multiples of 90 degrees.
+    def __init__(self):
+        pass
 
+    def __call__(self, sample):
+        return torch.rot90(sample, k=random.randrange(4), dims=[1, 2])
 
 # Setup tensorboard
 tb_parent_dir = './runs/'
@@ -70,39 +99,37 @@ sha = repo.head.object.hexsha
 local_branch = repo.active_branch.name
 run_dir = local_branch + '/' + sha[-3:] + '/' +  datetime.datetime.now().isoformat(timespec='seconds') + '/'
 print('TB Log Directory is: ', tb_parent_dir + run_dir)
-#writer = SummaryWriter(log_dir=tb_parent_dir + run_dir)
+writer = SummaryWriter(log_dir=tb_parent_dir + run_dir)
 
 # Setup model saving
 model_parent_dir = './model_checkpoints/'
 model_checkpoint_dir = model_parent_dir + local_branch + '/'
-path = Path(model_checkpoint_dir)
-path.mkdir(parents=True, exist_ok=True)
 
 # ## Set Backend
 backend_name = "torch"
-#backend_name = "torch.cuda.float32"
-#backend_name = "torch.cuda.float64"
 fdtd.set_backend(backend_name)
 if(backend_name.startswith("torch.cuda")):
     device = "cuda"
 else:
     device = "cpu"
 
-ih, iw = (180, 180)
-image_transform = torchvision.transforms.Compose([torchvision.transforms.Resize((ih, iw)),
-                               torchvision.transforms.ToTensor()])
+image_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Resize((args.image_size*args.image_scaler, args.image_size*args.image_scaler))])
 
+
+ih, iw = (args.image_size*args.image_scaler, args.image_size*args.image_scaler)
 
 # Physics constants
 WAVELENGTH = 1550e-9 # meters
 SPEED_LIGHT: float = 299_792_458.0  # [m/s] speed of light
 GRID_SPACING = 0.1 * WAVELENGTH # meters
 
-
 # Size of grid boundary layer
-bw = 10
+bw = 10*args.image_scaler
 # Create FDTD Grid
 grid_h, grid_w = (ih+bw*2, iw+bw*2)
+print('Grid height and width: ', grid_h, grid_w)
 # Boundaries with width bw
 grid = fdtd.Grid(
     (grid_h, grid_w, 1),
@@ -111,17 +138,13 @@ grid = fdtd.Grid(
     permeability=1.0,
 )
 
-# Slow down the grid!
-time_scaler = 1
-print('Grid time step: ', grid.time_step)
-grid.time_step = grid.time_step / time_scaler
-print('Grid time step: ', grid.time_step)
-
 # Calculate how long it takes a wave to cross the entire grid.
 grid_diag_cells = math.sqrt(grid_h**2 + grid_w**2)
 grid_diag_len = grid_diag_cells * GRID_SPACING
 grid_diag_steps = int(grid_diag_len/SPEED_LIGHT/grid.time_step)+1
 print('Time Steps to Cover Entire Grid: ', grid_diag_steps)
+# The number of steps is based on the coverage ratio.
+em_steps = int(grid_diag_steps*args.coverage_ratio)
 
 
 # Create learnable objects at the boundaries
@@ -139,21 +162,19 @@ grid[bw:bw+ih,bw:bw+iw,0] = fdtd.CorticalColumnPlaneSource(
 )
 
 # Object defining the cortical column substrate 
-grid[bw:-bw, bw:-bw, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, name="cc_substrate")
-
+grid[bw:-bw, bw:-bw, :] = fdtd.LearnableAnisotropicObject(permittivity=2.5, is_substrate=True, name="cc_substrate")
 # List all model checkpoints
 checkpoints = [f for f in listdir(model_checkpoint_dir) if(isfile(join(model_checkpoint_dir, f)) and f.endswith('.pt'))]
-# Get the latest checkpoint
-model = AutoEncoder(grid=grid, num_em_steps=405, input_chans=3, output_chans=3).to(device)
-if(args.load_file is not None):
-    start_step = int(args.load_file.split('/')[-1].split('_')[-1].split('.')[0])
-    print('Loading model {0}. Starting at step {1}.'.format(args.load_file, start_step))
-    grid_path = args.load_file.rsplit('.', 1)[0] + '.grd'
-    model.load_state_dict(torch.load(args.load_file))
-else:
-    print('No model provided, exiting...')
-    sys.exit()
 
+torch.autograd.set_detect_anomaly(True)
+# The weights for the reconstruction loss at each em time step. 
+loss_step_weights = torch.ones(em_steps)/em_steps
+#loss_step_weights = torch.nn.Parameter(torch.reshape(loss_step_weights, (-1, 1, 1, 1, 1)))
+loss_step_weights.requires_grad = True
+softmax = torch.nn.Softmax(dim=0)
+
+# Initialize the model and grid with default params.
+model = AutoEncoder(num_em_steps=em_steps, grid=grid, input_chans=3, output_chans=3).to(device)
 print('All grid objects: ', [obj.name for obj in grid.objects])
 grid_params_to_learn = []
 grid_params_to_learn += [get_object_by_name(grid, 'xlow').inverse_permittivity]
@@ -167,46 +188,119 @@ grid_params_to_learn += [get_object_by_name(grid, 'cc_substrate').nonlin_conv.bi
 # Nonlinearity weights for the cortical columns. 
 grid_params_to_learn += [get_source_by_name(grid, 'cc').nonlin_conv.weight]
 grid_params_to_learn += [get_source_by_name(grid, 'cc').nonlin_conv.bias]
+# The weights for the loss.
+grid_params_to_learn += [loss_step_weights]
+# Load saved params for model and optimizer.
+checkpoint_steps = [int(cf.split('_')[-1].split('.')[0]) for cf in checkpoints]
+if(args.load_file is not None):
+    start_step = int(args.load_file.split('/')[-1].split('_')[-1].split('.')[0])
+    print('Loading model {0}. Starting at step {1}.'.format(args.load_file, start_step))
+    optimizer_path = args.load_file.rsplit('.', 1)[0] + '.opt'
+    grid_path = args.load_file.rsplit('.', 1)[0] + '.grd'
+    model.load_state_dict(torch.load(args.load_file))
+else:
+    if(args.load_step == 'latest'):
+        if(len(checkpoint_steps) > 0):
+            latest_idx = np.argmax(checkpoint_steps)
+            start_step = checkpoint_steps[latest_idx]
+            model_dict_path = model_checkpoint_dir + checkpoints[latest_idx]
+            optimizer_path = model_dict_path.rsplit('.', 1)[0] + '.opt'
+            grid_path = model_dict_path.rsplit('.', 1)[0] + '.grd'
+            print('Loading model {0} with optimizer {1} and grid {2}.'.format(model_dict_path, optimizer_path, grid_path))
+            model.load_state_dict(torch.load(model_dict_path))
+        else:
+            start_step = 0
+    elif(int(args.load_step) != 0):
+        if(int(args.load_step) not in checkpoint_steps):
+            print('Checkpoint {0} not found in {1}'.format(args.load_step, model_checkpoint_dir))
+            sys.exit()
+        start_step = int(args.load_step)
+        model_idx = np.where(np.array(checkpoint_steps) == start_step)[0][0]
+        model_dict_path = model_checkpoint_dir + checkpoints[model_idx]
+        optimizer_path = model_dict_path.rsplit('.', 1)[0] + '.opt'
+        grid_path = model_dict_path.rsplit('.', 1)[0] + '.grd'
+        print('Loading model {0} with optimizer {1} and grid {2}.'.format(model_dict_path, optimizer_path, grid_path))
+        model.load_state_dict(torch.load(model_dict_path))
+    else:
+        print('Starting model at step 0')
+        start_step = 0
+        optimizer_path = None
+        grid_path = None
 
-# Load the grid params from file.
-if(grid_path is not None):
+def toy_img(img):
+    img = torch.zeros_like(img)
+    x, y, b, s = np.random.rand(4)
+    max_size = 14
+    min_size =  6
+    max_b = 1.0
+    min_b = 0.5
+    x = int(x*(img.shape[-1] - max_size))
+    y = int(y*(img.shape[-2] - max_size))
+    b = float(min_b + b*(max_b - min_b))
+    s = int(min_size + s*(max_size - min_size))
+    img[..., x:x+s, y:y+s] = b
+    return bd.array(img[:,0,...])
+
+reset_optimizer = False
+if((grid_path is not None)):
     print('Loading grid params...')
     with torch.no_grad():
         load_grid_params_to_learn = torch.load(grid_path)
         for idx, tensor in enumerate(load_grid_params_to_learn):
-            grid_params_to_learn[idx][...] = tensor[...]
+            #if(args.image_scaler == 1 or tensor.shape == grid_params_to_learn[idx][...].shape):
+            if(tensor.shape == grid_params_to_learn[idx][...].shape):
+                grid_params_to_learn[idx][...] = tensor[...]
+            else:
+                if(idx == len(load_grid_params_to_learn) - 1):
+                    tensor = torch.squeeze(tensor)
+                # Interpolate the thing....
+                print('INFO: Shapes are mismatched: {0} vs {1}'.format(tensor[...].shape, grid_params_to_learn[idx][...].shape))
+                
+                # If this is a grid param, expand it over the spatial dims.
+                if(len(tensor.shape) > 1):
+                    reps = np.ones(len(tensor.shape), dtype=int)
+                    for i in range(len(reps)):
+                        reps[i] = args.image_scaler
+                        if(i >= 1):
+                            break
 
-# Optimizer params
-mse = torch.nn.MSELoss(reduce=False)
-loss_fn = torch.nn.MSELoss()
+                    tensor_np_interp = scipy.ndimage.zoom(tensor.detach().numpy(), reps, order=1)
+                    grid_params_to_learn[idx][...] = torch.from_numpy(tensor_np_interp)
+                    print('INFO: Grid object scaled to shape: ', grid_params_to_learn[idx][...].shape)
+                # If this is the loss step weights, scale it linearly to fit the new size.
+                else:
+                    tensor_interp = torch.nn.functional.interpolate(tensor[None, None, ...], grid_params_to_learn[idx][...].shape, mode='linear')
+                    grid_params_to_learn[idx][...] = tensor_interp
+                    print('INFO: EM Step loss object scaled to shape: ', grid_params_to_learn[idx][...].shape)
+                    print('EM Steps: ', em_steps)
+                    print('Loss step weights: ', loss_step_weights.shape)
 
-em_steps = model.num_em_steps
+                # Since parameter shapes have changed, the optimizer weights are obsolete.
+                reset_optimizer = True
+
+# Combine grid and model params and register them with the optimizer.
+
+with torch.inference_mode():
+    # Add images to tensorboard
+    for img_idx, img_file in enumerate(img_paths):
+        #img = cv2.imread(img_file)
+        img = Image.open(img_file)
+        img = image_transform(img)[None, ...]
+        print(img_file, img.shape)
+        # Reset grid
+        grid.reset()
+        for em_step, (img_hat_em, em_field) in enumerate(model(img)):
+            print('Generating image for illusion {0} and em step {1}'.format(img_idx, em_step), end='\r')
+            # Process outputs
+            e_field_img = em_field[0:3,...]
+            h_field_img = em_field[3:6,...]
+            # Write to TB
+            img_grid = torchvision.utils.make_grid([img[0,...], img_hat_em,
+                norm_img_by_chan(e_field_img), 
+                norm_img_by_chan(h_field_img)])
+            #img_grid = torchvision.transforms.functional.resize(img_grid, size=(img_grid.shape[1] * 4, img_grid.shape[2] * 4), interpolation=torchvision.transforms.InterpolationMode.NEAREST)
+
+            #writer.add_image('sample', img_grid, em_step)
+            save_image(img_grid, './images/img_p{0}_idx{1}.png'.format('{0:06.3f}'.format(img_idx), str(em_step).zfill(12)))
 
 
-powers = np.exp(np.linspace(0.1, 3.0, 12)) - 1.0
-
-# Add images to tensorboard
-for img_idx, img_file in enumerate(img_paths):
-    #img = cv2.imread(img_file)
-    img = Image.open(img_file)
-    img = image_transform(img)[None, ...]
-    print(img.shape)
-    # Reset grid
-    grid.reset()
-
-    for em_step, (img_hat_em, em_field) in enumerate(model(img, amp_scaler=1.0)):
-        print('Generating image for power {0} and em step {1}'.format(img_idx, em_step))
-        # Process outputs
-        e_field_img = em_field[0:3,...]
-        h_field_img = em_field[3:6,...]
-        # Write to TB
-        img_grid = torchvision.utils.make_grid([img[0,...], img_hat_em,
-            norm_img_by_chan(e_field_img), 
-            norm_img_by_chan(h_field_img)])
-        print('Generating image: ', em_step)
-        #img_grid = torchvision.transforms.functional.resize(img_grid, size=(img_grid.shape[1] * 4, img_grid.shape[2] * 4), interpolation=torchvision.transforms.InterpolationMode.NEAREST)
-
-        #writer.add_image('sample', img_grid, em_step)
-        save_image(img_grid, './images/illusion_{0}_idx_{1}.png'.format(img_idx, str(em_step).zfill(12)))
-
-#writer.close()
